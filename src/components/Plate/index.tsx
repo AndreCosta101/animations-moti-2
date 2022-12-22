ae240229fd0eeb7c64252eb80976b3a439d77006
AndreCosta <andre.costa101@gmail.com>
2022-12-22 16:36:37 -0300

    animation with moti

--- src/components/Plate/index.tsx
+++ src/components/Plate/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import { MotiImage } from 'moti';
+
 import { styles } from './styles';
 import plateImg from '../../assets/plate.png';
 
 export function Plate() {
   return (
-    <Image
+    <MotiImage
       style={styles.plate}
       source={plateImg}
       resizeMode="contain"
+      from={{
+        rotate: '0deg',
+        opacity: 0
+      }}
+      animate={{
+        rotate: '100deg',
+        opacity: 1
+      }}
+      transition={{
+        type: 'timing',
+        duration: 2000,
+        // repeat: 3,
+        // loop: true
+        // repeatReverse: false
+      }}
     />
   );
 }
